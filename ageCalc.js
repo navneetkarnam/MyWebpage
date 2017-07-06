4d7b31f23c90d8aea2d9d7a576ab11dee18dbe67
Navneet Karnam <navneetkarnam@gmail.com>
2017-07-05 20:35:20 -0400

    more exercise files

---
+++ ageCalc.js
@@ -0,0 +1,3 @@
+var age = prompt("What is your age?");
+var days = 365.25 * age;
+alert(age+" years is roughly " + days + " " + "days");
---
+++ jsex1.js
@@ -0,0 +1,5 @@
+var fName = prompt("What is your First name?");
+var lName = prompt("What is your Last name?");
+var age = prompt("What is your age?");
+console.log("Your full name is " + fName + " " + lName);
+console.log("you are " + age + " years old");
